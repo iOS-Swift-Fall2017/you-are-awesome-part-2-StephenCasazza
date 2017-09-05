dd22fdfb38b3bbaefb13e17a58dc772aac26b5e4
Stephen Casazza <stephencasazza@raymonds-mbp.bc.edu>
2017-09-05 15:33:28 -0400

    Simulator not working, submitting the code that was working before my simulator crashed.

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
+    // This code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+       
+        let messages = ["You Are Fantastic!",
+                         "You Are Great!",
+                         "You Are Fantastic!",
+                         "Fabulous? That's You!",
+                         "You Brighten My Day Everyday!",
+                         "You Da Bomb",
+                         "You Are the Best ",
+                         "You Are Awesome"]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        } while index == newIndex
+        
+        index = newIndex
+        messageLabel.text = messages[index]
+        
+        
+//        var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+//        messageLabel.text = messages[randomIndex]
+        
+//        messageLabel.text = messages[index]
+//        index = index + 1
+//
+//        if index == messages.count {index = 0}
+    
+        
+//        let message1 = "You Are Sick!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//        let message4 = "You Are Fabulous!"
+//
+//        if awesomeLabel.text == message1 {
+//            awesomeLabel.text = message2
+//            awesomeLabel.textColor = UIColor.green
+//        } else if
+//            awesomeLabel.text == message2 {
+//            awesomeLabel.text = message3
+//            awesomeLabel.textColor = UIColor.blue
+//        } else if
+//            awesomeLabel.text == message3 {
+//            awesomeLabel.text = message4
+//            awesomeLabel.textColor = UIColor.orange
+//        } else {
+//            awesomeLabel.text = message1
+//            awesomeLabel.textColor = UIColor.red }
+ 
+ }
+ 
+            }
 
-
-}
 
